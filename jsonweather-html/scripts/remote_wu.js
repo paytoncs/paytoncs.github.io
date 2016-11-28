f3d1a16cc6648afeb2d919f2a97f28fb03a0f0ec
paytoncs <smi13169@byui.edu>
2016-11-28 10:28:36 -0700

    set

--- jsonweather-html/scripts/remote_wu.js
+++ jsonweather-html/scripts/remote_wu.js
@@ -58,20 +58,3 @@ $(function () {
         });
     }
 });
-
-
-
-
-
-
-      $("#cover").fadeOut(250);
-    }
-           });
-
-  }
-
-  // A function for changing a string to TitleCase
-  function toTitleCase(str){
-    return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-  }
-});
